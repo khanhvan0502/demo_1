d1858e14c54e8e2db90e59624ef0d6cca9af31a5
khanhvan0502 <89264464+khanhvan0502@users.noreply.github.com>
2021-11-11 13:41:06 +0700

    init project

---
+++ Vidu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Vidu extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Vidu;
